9db7fa623e65e0e1190d0ffa7fe65cc68eba0815
Israel Roldan <israel.alberto.rv@gmail.com>
2019-03-01 01:32:18 -0600

    Added files for runing docker for the containers as database, phpmyadmin, and web.

--- src/docker_create_container.sh
+++ src/docker_create_container.sh
@@ -2,28 +2,37 @@
 
 set +vxe
 
-#PROJECT=python_ecommerce
-#APP=app
-#ENVIRONMENT=dev
-
 usage() {
 	echo "Example:"
-	echo "PROJECT:      python_ecommerce"
-	echo "APP:          mySql"
-	echo "ENVIRONMENT:  dev"
-	echo "Usage: $0 -p project_name -a app_name -e enviroment_name" 1>&2; exit -1;
+	echo "PROJECT:      -n : python_ecommerce"
+	echo "APP:          -a : mySql"
+	echo "LABEL:        -z : dev"
+	echo ""
+	echo "Usage:"
+	echo "$0 \\
+	-n project_name \\
+	-a app_name \\
+	-z enviroment_name" 1>&2; exit -1;
 }
 
-while getopts ":p:a:e:" OPTIONS; do
+while getopts ":n:a:z:e:p:" OPTIONS; do
 	case "${OPTIONS}" in
-		p)
+		n)
 			PROJECT=${OPTARG}
 			;;
 		a)
 			APP=${OPTARG}
 			;;
+		z)
+			LABEL=${OPTARG}
+			;;
 		e)
-			ENVIRONMENT=${OPTARG}
+			ENVIRONMENT_VAR=${OPTARG}
+			ENVIRONMENT_VARS="${ENVIRONMENT_VARS} ${ENVIRONMENT_VAR}"
+			;;
+		p)
+			PORT=${OPTARG}
+			PORTS="${PORTS} ${PORT}"
 			;;
 		*)
 			usage
@@ -34,16 +43,11 @@ done
 # Delete all the parameters except the script name.
 shift $((OPTIND-1))
 
-if [ -z "${PROJECT}" ] || [ -z "${APP}" ] || [ -z "${ENVIRONMENT}" ]; then
+if [ -z "${PROJECT}" ] || [ -z "${APP}" ] || [ -z "${LABEL}" ]; then
 	usage
 fi
 
 
-
-set -vxe
-
-DIR=./${APP}
-DOCKER_PATH=${DIR}/docker
 
 # Docker: Purging All Unused or Dangling Images, Containers, Volumes, and Networks.
 #docker system prune -a
@@ -51,54 +55,68 @@ DOCKER_PATH=${DIR}/docker
 # Delete all.
 #docker service rm $(docker service ls -q)
 #docker network rm $(docker network ls -q -f name=python_ecommerce_app_dev_service_webnet)
-#docker container stop $(docker ps -aq)
+#docker container stop $(docker ps -aq) && docker container rm $(docker ps -aq)
 #yes | docker container prune
-#docker image rm ${PROJECT}_${APP}:${ENVIRONMENT}
+#docker image rm ${PROJECT}_${APP}:${LABEL}
 #yes | docker image prune
 
 # Show the all.
-docker container ls --all
-docker image ls --all
-docker service ls
-docker network ls
+#docker container ls --all
+#docker image ls --all
+#docker service ls
+#docker network ls
+
+set -vxe
+
+DIR=./${APP}
+DOCKER_PATH=${DIR}/docker
 
 # Build the image.
 docker build \
 	--file ${DOCKER_PATH}/Dockerfile \
-	--tag ${PROJECT}_${APP}:${ENVIRONMENT} \
+	--tag ${PROJECT}_${APP}:${LABEL} \
 	${DIR}/
 
+set +vxe
+
 # Run the image to create a container.
-docker run \
-	--detach \
-	--publish 80:80 \
-	${PROJECT}_${APP}:${ENVIRONMENT}
+#docker run \
+#	--detach \
+#	--name ${PROJECT}_${APP}_${LABEL} \
+#	$(
+#		for ENVIRONMENT_VAR in $ENVIRONMENT_VARS; do
+#			echo "--env ${ENVIRONMENT_VAR} "
+#		done   
+#	) \
+#	$(
+#		for PORT in $PORTS; do
+#			echo "--publish ${PORT} "
+#		done   
+#	) \
+#	${PROJECT}_${APP}:${LABEL}
 
 # Deployment
-docker swarm leave --force
-docker swarm init
-docker stack deploy -c ${DOCKER_PATH}/docker-compose.yml ${PROJECT}_${APP}_${ENVIRONMENT}_service
-#docker stack rm ${PROJECT}_${APP}_${ENVIRONMENT}_service
+#docker swarm leave --force
+#docker swarm init
+#docker stack deploy -c ${DOCKER_PATH}/docker-compose.yml ${PROJECT}_${APP}_${LABEL}_service
+#docker stack rm ${PROJECT}_${APP}_${LABEL}_service
 
-docker stack services ${PROJECT}_${APP}_${ENVIRONMENT}_service
-docker service ps ${PROJECT}_${APP}_${ENVIRONMENT}_service_web
-docker container ls -q
+#docker stack services ${PROJECT}_${APP}_${LABEL}_service
+#docker service ps ${PROJECT}_${APP}_${LABEL}_service_web
 
 # Show the all.
-docker container ls --all
-docker image ls --all
-docker service ls
-docker network ls
+#docker container ls --all
+#docker image ls --all
+#docker service ls
+#docker network ls
 
 # Docker using remote repositories
 #docker login
-#docker tag ${PROJECT}_${APP}:${ENVIRONMENT} airvzxf/${PROJECT}_${APP}:${ENVIRONMENT}
-#docker push airvzxf/${PROJECT}_${APP}:${ENVIRONMENT}
+#docker tag ${PROJECT}_${APP}:${LABEL} airvzxf/${PROJECT}_${APP}:${LABEL}
+#docker push airvzxf/${PROJECT}_${APP}:${LABEL}
 
 # Run the remote image to create a container
 #docker run \
 #	--detach \
 #	--publish 80:80 \
-#	airvzxf/${PROJECT}_${APP}:${ENVIRONMENT}
-
-set +vxe
+#	airvzxf/${PROJECT}_${APP}:${LABEL}
--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,24 @@
 #!/bin/bash -xve
 
-#./docker_create_container.sh -p python_ecommerce -a database -e dev
+PROJECT=python_ecommerce
+LABEL=dev
 
-./docker_create_container.sh -p python_ecommerce -a web -e dev
+
+APP=database
+./docker_create_container.sh -n ${PROJECT} -z ${LABEL} -a ${APP}
+#docker exec -it python_ecommerce_database_dev bash
+#ALTER USER dev_user IDENTIFIED WITH mysql_native_password BY '111';
+
+APP=phpmyadmin
+./docker_create_container.sh -n ${PROJECT} -z ${LABEL} -a ${APP}
+
+
+APP=web
+./docker_create_container.sh -n ${PROJECT} -z ${LABEL} -a ${APP}
+
+
+#clr && docker service rm $(docker service ls -q) && docker container stop $(docker ps -aq) && docker container rm $(docker ps -aq)
+#clr && docker container stop $(docker ps -aq) && docker container rm $(docker ps -aq)
+#docker stack deploy -c ./docker-compose-dev.yml hola
+
+docker-compose -f ./docker-compose-dev.yml up
