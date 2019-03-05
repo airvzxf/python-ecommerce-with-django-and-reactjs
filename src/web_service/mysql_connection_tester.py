a0e7956dc3f91732b50a50340f4af6176be8ef5f
Israel Roldan <israel.alberto.rv@gmail.com>
2019-03-05 14:26:12 -0600

    Setup the web service which wait until the database is up.

---
+++ src/web_service/mysql_connection_tester.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import mysql.connector
+from mysql.connector import errorcode
+
+cnx = None
+
+config = {
+    'user': 'dev_user',
+    'password': '111',
+    'host': '127.0.0.1',
+    'port': '3306',
+    'database': 'python_ecommerce',
+    'raise_on_warnings': True
+}
+
+
+try:
+    cnx = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+    exit(-1)
+
+cursor = cnx.cursor()
+
+cursor.execute("SHOW TABLES;")
+
+for (table_name,) in cursor:
+    print(table_name)
+
+cursor.close()
+cnx.close()
--- src/web_service/run.sh
+++ src/web_service/run.sh
@@ -1,4 +1,25 @@
-#!/bin/bash -xve
+#!/bin/bash -e
 
-sed -i 's/127.0.0.1/db/g' x.py
-sed -i "s/'HOST': '127.0.0.1'/'HOST': 'db'/g" api/settings.py
+HOST="$1"
+PORT="$2"
+
+DB_HOST="$3"
+DB_PORT="$4"
+
+sed -i "s/'host': '127.0.0.1'/'host': '"${DB_HOST}"'/g" mysql_connection_tester.py
+sed -i "s/'port': '3306'/'port': '"${DB_PORT}"'/g" mysql_connection_tester.py
+
+sed -i "s/'HOST': '127.0.0.1'/'HOST': '"${DB_HOST}"'/g" api/settings.py
+sed -i "s/'PORT': '127.0.0.1'/'PORT': '"${DB_PORT}"'/g" api/settings.py
+
+until python mysql_connection_tester.py -c '\q'; do
+  sleep 3
+done
+
+echo "MySQL is up - executing command"
+
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'israel.alberto.rv@gmail.com', 'Python1234Ecommerce!')" | python manage.py shell
+
+python manage.py runserver ${HOST}:${PORT}
